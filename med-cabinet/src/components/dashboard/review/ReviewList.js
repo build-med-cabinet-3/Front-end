da5e5c08c6c56614598cfce1f3a547082f341b85
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-11-19 09:50:01 -0800

    cleaned up comments

--- med-cabinet/src/components/dashboard/review/ReviewList.js
+++ med-cabinet/src/components/dashboard/review/ReviewList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReviewCard from "./ReviewCard";
+import ReviewForm from "./ReviewForm";
 
 function ReviewList(review) {
   return (
-    <div>
+    <div className="review-list-container">
+      <ReviewForm />
       <ReviewCard />
       {/* {review.map((review, index) => (
         <ReviewCard
