be39bec822274d90f0d0c4b2dc88225d58473d98
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-11-18 23:28:15 -0800

    added private route to recommendations and reviews from the dashboard.

--- med-cabinet/src/components/WelcomePage.js
+++ med-cabinet/src/components/WelcomePage.js
@@ -7,6 +7,8 @@ import PrivateRoute from "../utils/PrivateRoute";
 import Login from "./Login";
 import Signup from "./Signup";
 import Dashboard from "../components/dashboard/Dashboard";
+import ReviewList from "../components/dashboard/review/ReviewList";
+import RecommendList from "../components/dashboard/recommend/RecommendList";
 
 export default function WelcomePage() {
   return (
@@ -16,6 +18,12 @@ export default function WelcomePage() {
         <Route path="/login" render={props => <Login {...props} />} />
         <Route path="/signup" render={props => <Signup {...props} />} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/dashboard/reviews" component={ReviewList} />
+        <PrivateRoute
+          exact
+          path="/dashboard/recommendations"
+          component={RecommendList}
+        />
       </Switch>
     </div>
   );
