69a4f117c781345de406ba7640238a904b739ee7
Robert Campbell <robertc1191@Roberts-MacBook-Pro-2.local>
2019-11-21 09:47:11 -0800

    minor fixes recommendations

--- src/components/dashboard/recommend/RecommendList.js
+++ src/components/dashboard/recommend/RecommendList.js
@@ -6,9 +6,9 @@ import RecommendForm from "./RecommendForm";
 import RecommendCard from "./RecommendCard";
 
 export const RecommendList = ({ getRecList, recommendList }) => {
-  if (!recommendList) {
-    return <h1>Loading...</h1>;
-  }
+  // if (!recommendList) {
+  //   return <h1>Loading...</h1>;
+  // }
   return (
     <div className="recommend-container">
       <RecommendForm />
